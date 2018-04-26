20c4374c33f10d05997d48b70234472afafa4df9
Pierrick MADE <pierrick.made@epita.fr>
2018-04-26 18:53:43 +0200

    php/js / CAPZ description / index buttons

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -18,8 +18,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'pierrick.made@epita.fr';
 $email_subject = "PEnuts Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n";
+$email_body = "Here is a message from PEnuts contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: pierrick.pers@gmail.com\n";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;         
